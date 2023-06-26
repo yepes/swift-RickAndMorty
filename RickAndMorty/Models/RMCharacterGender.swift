8b12e1c6a4b337eb21fffbfde7955bc682d2722b
Jesus Yepes <jesus@jesusyepes.com>
2023-06-26 19:41:56 +0200

    chapter 3

---
+++ RickAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,15 @@
+//
+//  RMCharacterGender.swift
+//  RickAndMorty
+//
+//  Created by Jesus Yepes on 26/6/23.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case male = "male"
+    case female = "female"
+    case genderles = "genderless"
+    case `unknown` = "unknown"
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,12 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Jesus Yepes on 26/6/23.
+//
+
+import Foundation
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
